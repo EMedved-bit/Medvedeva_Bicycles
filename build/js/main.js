e0c91d025d657651de3e5da59141943c673673f9
EMedved-bit <eymedvedeva.spb@gmail.com>
2021-10-24 18:14:18 +0300

    Правки по замечаниям

--- build/js/main.js
+++ build/js/main.js
@@ -13,6 +13,9 @@ const layer = L.tileLayer(LAYER_TEMPLATE, {
 const nojsElements = document.querySelectorAll('.nojs');
 const menuOpenButton = document.querySelector('.header-nav__toggle');
 const navMenu = document.querySelector('.header-nav__list');
+const menuLinks = document.querySelectorAll('.header-nav__link');
+const phoneInput = document.querySelector('#phone');
+const iMask = window.IMask;
 
 nojsElements.forEach((element) => {
   element.classList.remove('nojs');
@@ -23,12 +26,25 @@ if (menuOpenButton && navMenu) {
     if (menuOpenButton.classList.contains('header-nav__toggle--opened')) {
       menuOpenButton.classList.remove('header-nav__toggle--opened');
       navMenu.classList.remove('header-nav__list--opened');
+      document.body.style.overflowY = 'visible';
     } else {
       menuOpenButton.classList.add('header-nav__toggle--opened');
       navMenu.classList.add('header-nav__list--opened');
+      document.body.style.overflowY = 'hidden';
     }
   });
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', function () {
+      menuOpenButton.classList.remove('header-nav__toggle--opened');
+      navMenu.classList.remove('header-nav__list--opened');
+      document.body.style.overflowY = 'visible';
+    });
+  });
 }
+
+iMask(phoneInput, {
+  mask: '+{7}(000)000-00-00'
+});
 
 layer.addTo(map);
 
